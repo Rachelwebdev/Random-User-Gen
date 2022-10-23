49c5c57c50193351e9217e7b1617e478f3fbeefc
Rachel Isaac <rachelisaac995@gmail.com>
2022-10-23 21:54:24 +0400

    edited the landing page styles

--- src/Component/Landing.js
+++ src/Component/Landing.js
@@ -1,7 +1,27 @@
 import React from "react";
+import "./Landing.css";
+import { Link } from "react-router-dom";
 
 function Landing() {
-  return <h1>Login Form</h1>;
+  return (
+    <>
+      <div className="main-container">
+        <h1 className="header-text">Login to See Random Users</h1>
+        <div className="formContainer">
+          <input type="text" name="email" id="email" placeholder="Email" />
+          <input
+            type="text"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+          <Link to="/User" className="btn-submit" type="submit">
+            Sign In
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Landing;
