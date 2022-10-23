f2c62e0194841388fc1b7a3a9721bfbc834f9222
Rachel Isaac <rachelisaac995@gmail.com>
2022-10-23 21:54:07 +0400

    installed browser router and created nested routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Landing from "./Component/Landing";
+import User from "./Component/User";
 
 function App() {
   return (
     <div className="App">
-      <Landing />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/User" element={<User />} />
+      </Routes>
     </div>
   );
 }
