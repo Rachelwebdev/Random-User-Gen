94d824ed8fa983af999b7110331dda00e8490697
Rachel Isaac <rachelisaac995@gmail.com>
2022-10-23 23:29:49 +0400

    built and styled User component

--- src/Component/User.js
+++ src/Component/User.js
@@ -7,7 +7,20 @@ export default function User() {
       <div className="main-user-container">
         <h1 className="title-heading">HELLO, I AM A NEW USER 🧑</h1>
         <div className="user-box">
-          <h1>User data go in here</h1>
+          <img
+            className="user-image"
+            src="https://randomuser.me/api/portraits/women/3.jpg"
+            alt="User-profile"
+          />
+
+          <div className="user-info">
+            <h2>Name: Karl Johnson </h2>
+            <h2>Email: karl.johnson@example.com</h2>
+            <h2>Birthday: 1966-12-17 </h2>
+            <h2>Address: 6057 Avondale Ave</h2>
+            <h2>Phone-No: (268) 420-4900</h2>
+            <h2>Password: luan </h2>
+          </div>
         </div>
         <div className="buttons">
           <button className="prev">PREV</button>
@@ -18,7 +31,6 @@ export default function User() {
             <button className="num">4</button>
             <button className="num">5</button>
           </div>
-
           <button className="next">NEXT</button>
         </div>
       </div>
