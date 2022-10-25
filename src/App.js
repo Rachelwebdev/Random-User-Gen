e0398eed6f4a40923825691bafd9ec6c5c39a2ac
Rachel Isaac <rachelisaac995@gmail.com>
2022-10-25 20:28:52 +0400

    imported newly built components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import Landing from "./Component/Landing";
 import PaginationContainer from "./Component/PaginationContainer";
 import Navbar from "./Component/Navbar";
+import About from "./Component/About";
+import Register from "./Component/Register";
+import Footer from "./Component/Footer";
 
 function App() {
   return (
@@ -10,8 +13,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/About" element={<About />} />
         <Route path="/User" element={<PaginationContainer />} />
+        <Route path="/Register" element={<Register />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
